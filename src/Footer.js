943511389258d2e6507e8324f9c3661674478957
Шаврова Марина Петровна <maryna.shavrova@privatbank.ua>
2022-07-21 11:40:42 +0300

    Added weather project with React

--- src/Footer.js
+++ src/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
   <footer>
     This project was coded for 
     <a className="link" href="https://www.shecodes.io/" target="_blank" rel="neoprenes preferrer"> SheCodes</a> and is 
-    <a  className="link" href="https://github.com/MarinaShavrova/SheCodesMain" target="_blank" rel="neoprenes preferrer">open-sourced on GitHub</a> and hosted on Netlify.           
+    <a  className="link" href="https://github.com/MarinaShavrova/weatherReact" target="_blank" rel="neoprenes preferrer">open-sourced on GitHub</a> and hosted on Netlify.           
     </footer>
     </>
   );
